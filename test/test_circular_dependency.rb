e1a0b3e8dd712a7653249210a1cedafd71d7bf8e
upinetree <upinetree@gmail.com>
2019-11-13 02:13:27 +0900

    Support a simple circular dependencies rule

---
+++ test/test_circular_dependency.rb
@@ -0,0 +1,22 @@
+require "minitest/autorun"
+require "depcop"
+
+class TestCircularDependency < MiniTest::Test
+  def test_showing_circles
+    nodes = [["A"], ["A", "B"], ["C"], ["D"]]
+    edges = [
+      { "from" => ["A"], "to" => ["A", "B"] },
+      { "from" => ["C"], "to" => ["D"] },
+      { "from" => ["D"], "to" => ["A"] },
+    ]
+    cyclic_edges = edges + [
+      { "from" => ["A"], "to" => ["C"] },
+    ]
+
+    warnings = Depcop::CircularDependency.new(nodes, edges).run
+    assert_equal 0, warnings.size
+
+    warnings = Depcop::CircularDependency.new(nodes, cyclic_edges).run
+    assert_equal 1, warnings.size
+  end
+end
