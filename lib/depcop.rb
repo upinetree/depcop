81e066971d632ce3df409209e419a4c04bc7ab78
upinetree <upinetree@gmail.com>
2020-02-15 17:14:20 +0900

    Short name

--- lib/depcop.rb
+++ lib/depcop.rb
@@ -5,8 +5,8 @@ require "json"
 
 module Depcop
   class Cli
-    def self.run(json_string)
-      nodes, edges = JSON.parse(json_string).values_at("nodes", "edges")
+    def self.run(json_str)
+      nodes, edges = JSON.parse(json_str).values_at("nodes", "edges")
 
       warnings = rules.map do |rule|
         rule.new(nodes, edges).run
